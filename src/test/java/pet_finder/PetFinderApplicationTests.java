95c6f2a0446bf9e121ccfa9d8454c2c1d23de5b6
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-05-30 12:48:05 -0300

    primer commit

---
+++ src/test/java/pet_finder/PetFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package pet_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
