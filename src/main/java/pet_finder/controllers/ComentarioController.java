a3dfca972384e4b589417f6b488c9cc9ec08e6b3
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-06-17 18:08:42 -0300

    cambiar manejo de miembroId en ComentarioController

--- src/main/java/pet_finder/controllers/ComentarioController.java
+++ src/main/java/pet_finder/controllers/ComentarioController.java
@@ -2,13 +2,17 @@ package pet_finder.controllers;
 import org.springframework.http.HttpStatus;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import pet_finder.config.MiembroUserDetails;
 import pet_finder.dtos.ComentarioDetailDTO;
 import pet_finder.dtos.ComentarioRequestDTO;
 import pet_finder.mappers.ComentarioMapper;
 import pet_finder.models.Comentario;
+import pet_finder.models.Miembro;
 import pet_finder.services.ComentarioServices;
+import pet_finder.services.MiembroService;
 
 import java.util.List;
 
@@ -17,20 +21,27 @@ import java.util.List;
 public class ComentarioController {
 
     private final ComentarioServices comentarioService;
+    private final MiembroService miembroService;
     private final ComentarioMapper comentarioMapper;
 
 
-    public ComentarioController(ComentarioServices comentarioService, ComentarioMapper comentarioMapper) {
+    public ComentarioController(ComentarioServices comentarioService, MiembroService miembroService, ComentarioMapper comentarioMapper) {
         this.comentarioService = comentarioService;
+        this.miembroService = miembroService;
         this.comentarioMapper = comentarioMapper;
     }
 
 
     @PostMapping
-    public ResponseEntity<ComentarioDetailDTO> crearComentario(@Valid @RequestBody ComentarioRequestDTO request) {
+    public ResponseEntity<ComentarioDetailDTO> crearComentario(@Valid @RequestBody ComentarioRequestDTO request,
+                                                               @AuthenticationPrincipal MiembroUserDetails userDetails) {
+
+        Long miembroId = userDetails.getId(); // se obtiene el ID del miembro loggeado
 
         Comentario comentario = comentarioMapper.aEntidad(request);
-        Comentario creado = comentarioService.crearComentario(comentario, request.getIdPublicacion(), request.getIdUsuario());
+
+        // Crear comentario, recibe el id del miembro y el id de la publicacion, los valida y los settea en la entidad
+        Comentario creado = comentarioService.crearComentario(comentario, request.getIdPublicacion(), miembroId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(new ComentarioDetailDTO(creado));
     }
