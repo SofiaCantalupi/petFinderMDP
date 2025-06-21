d4c61f8f913cc5043153210902f3f59143f9cf43
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-06-21 14:49:54 -0300

    mover validaciones de enums de PublicacionValidation a MascotaValidation

--- src/main/java/pet_finder/validations/MascotaValidation.java
+++ src/main/java/pet_finder/validations/MascotaValidation.java
@@ -2,6 +2,8 @@ package pet_finder.validations;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Component;
+import pet_finder.enums.EstadoMascota;
+import pet_finder.enums.TipoMascota;
 import pet_finder.models.Mascota;
 import pet_finder.repositories.MascotaRepository;
 
@@ -22,6 +24,26 @@ public class MascotaValidation {
     public void esActivo(Boolean activo) {
         if (Boolean.FALSE.equals(activo)) {
             throw new IllegalStateException("La mascota ya fue dada de baja.");
+        }
+    }
+
+    // Es utilizado para transformar el string que representa el tipo de mascota a su respectivo Enum.
+    // Si el valor (tipo) no es valido, se lanza una excepcion
+    public TipoMascota validarYConvertirTipoMascota(String tipoString){
+        try{
+            // Se trata de convertir el string a su equivalente Enum
+            return TipoMascota.valueOf(tipoString.toUpperCase());
+        }catch (IllegalArgumentException exc){
+            throw new IllegalArgumentException("Tipo de mascota inválido.");
+        }
+    }
+
+    // Es utilizado para transformar el string que representa el estado de una mascota a su respectivo Enum.
+    public EstadoMascota validarYConvertirEstadoMascota(String tipoString){
+        try {
+            return EstadoMascota.valueOf(tipoString.toUpperCase());
+        }catch (IllegalArgumentException exc){
+            throw new IllegalArgumentException("Estado de mascota inválido.");
         }
     }
 }
