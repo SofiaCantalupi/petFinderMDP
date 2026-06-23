763eb95480b1995f349c355308052ad42ede98eb
SofiaCantalupi <sofiacantalupi@gmail.com>
2026-06-22 23:47:18 -0300

    Modificar Ubicacion: persistencia de latitud y longitud. Actualizar validacion de Ubicacion

--- src/main/java/pet_finder/dtos/ubicacion/UbicacionRequestDTO.java
+++ src/main/java/pet_finder/dtos/ubicacion/UbicacionRequestDTO.java
@@ -2,11 +2,9 @@ package pet_finder.dtos.ubicacion;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-/**
- * @author Daniel Herrera
- */
 public class UbicacionRequestDTO {
 
         @NotBlank(message = "La direccion no puede estar vacia.")
@@ -16,17 +14,11 @@ public class UbicacionRequestDTO {
         @Min(value = 0, message = "La altura no puede ser negativa.")
         private Integer altura;
 
-        @NotBlank(message = "La ciudad no puede estar vacia.")
-        @Size(max = 50, message = "La dirección no puede tener más de 50 caracteres.")
-        private String ciudad;
+        @NotNull(message = "La latitud no puede ser null")
+        private Double latitud;
 
-        @NotBlank(message = "La region no puede estar vacia.")
-        @Size(max = 50, message = "La dirección no puede tener más de 50 caracteres.")
-        private String region; // Estado, provincia o departamento
-
-        @NotBlank(message = "El pais no puede estar vacio.")
-        @Size(max = 50, message = "La dirección no puede tener más de 50 caracteres.")
-        private String pais;
+        @NotNull(message = "La longitud no puede ser null")
+        private Double longitud;
 
         public UbicacionRequestDTO() {}
 
@@ -38,15 +30,11 @@ public class UbicacionRequestDTO {
                 return altura;
         }
 
-        public String getCiudad() {
-                return ciudad;
+        public @NotNull(message = "La latitud no puede ser null") Double getLatitud() {
+                return latitud;
         }
 
-        public String getRegion() {
-                return region;
-        }
-
-        public String getPais() {
-                return pais;
+        public @NotNull(message = "La longitud no puede ser null") Double getLongitud() {
+                return longitud;
         }
 }
--- src/main/java/pet_finder/validations/UbicacionValidation.java
+++ src/main/java/pet_finder/validations/UbicacionValidation.java
@@ -1,18 +1,13 @@
 package pet_finder.validations;
 
-import org.springframework.http.*;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 import pet_finder.dtos.ubicacion.UbicacionRequestDTO;
 import pet_finder.exceptions.UbicacionInvalidaException;
 import pet_finder.models.Ubicacion;
 
 import java.util.Objects;
 
-/**
- * @author Daniel Herrera
- */
+
 @Component
 public class UbicacionValidation {
 
@@ -20,59 +15,32 @@ public class UbicacionValidation {
 
     public void esInactivo(Ubicacion ubicacion){
         if (!ubicacion.getActivo()){
-            throw new IllegalStateException("La Ubicacion con ID : "+ubicacion.getId()+" esta inactiva.");
+            throw new IllegalStateException("La ubicación con ID : "+ubicacion.getId()+" esta inactiva.");
         }
     }
 
-    // todo: Comprobar funcionamiento de esta FUNCION
-    // FUNCION AUXILIAR VALIDA QUE LA UBICACION ENVIADA EXISTA REALMENTE
-    public void validarGeocodificacion(Ubicacion ubicacion) {
 
+    // Valida que las coordenadas (lat y long) no sean nulas, y que correspondan a la ciudad de Mar del Plata
+    public void validarGeocodificacion(Ubicacion ubicacion){
 
-        // Arma una cadena (ej: "Rivadavia 3470, Mar del Plata, Buenos Aires, Argentina")
-        String query = String.format("%s %s, %s, %s, %s",
-                ubicacion.getDireccion(),
-                ubicacion.getAltura(),
-                ubicacion.getCiudad(),
-                ubicacion.getRegion(),
-                ubicacion.getPais()
-        );
+        double latitud = ubicacion.getLatitud();
+        double longitud = ubicacion.getLongitud();
 
-        // Construye la URL para consultar al servicio Nominatim (OpenStreetMap)
-        String url = UriComponentsBuilder
-                .fromHttpUrl("https://nominatim.openstreetmap.org/search")
-                .queryParam("q", query)
-                .queryParam("format", "json")
-                .build()
-                .toUriString();
-
-        // Crea un cliente para hacer la petición HTTP
-        RestTemplate restTemplate = new RestTemplate();
-
-        // Configura la cabecera "User-Agent" (es obligatoria para que el servidor acepte la petición)
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("User-Agent", "mi-app");
-
-        // Crea una entidad HTTP con las cabeceras preparadas
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-
-        // Hace la consulta (GET) a la URL con las cabeceras
-        ResponseEntity<String> response = restTemplate.exchange(
-                url, HttpMethod.GET, entity, String.class
-        );
-
-        // La función devuelve true si:
-        // - La respuesta fue exitosa (status 200)
-        // - El cuerpo de la respuesta no es nulo
-        // - La respuesta no está vacía (es decir, encontró al menos una ubicación)
-        boolean ubicacionReal = response.getStatusCode() == HttpStatus.OK &&
-                response.getBody() != null &&
-                !response.getBody().equals("[]");
-
-        // SI NO se pudo geocodificar (direccion inexistente)
-        if (!ubicacionReal) {
-            throw new UbicacionInvalidaException("La Ubicacion '"+query+"' no se pudo localizar.");
+        if(ubicacion.getLatitud()== null || ubicacion.getLongitud() == null){
+            throw new UbicacionInvalidaException("La ubicación debe tener latitud y longitud.");
         }
+
+        double latMin = -38.15, latMax = -37.90;
+        double longMin = -57.70, longMax = -57.50;
+
+        boolean dentroDeMarDelPlata =
+                latitud >= latMin && latitud <= latMax &&
+                longitud >= longMin && longitud <= longMax;
+
+        if(!dentroDeMarDelPlata){
+            throw new UbicacionInvalidaException("Las coordenadas se encuentran fuera de Mar del Plata.");
+        }
+
     }
 
     //Este metodo se usa para indicar si dos ubicaciones son iguales para ser implementadas
@@ -81,10 +49,7 @@ public class UbicacionValidation {
         if (nueva == null) return false;
 
         return Objects.equals(original.getDireccion(), nueva.getDireccion())
-                && Objects.equals(original.getAltura(), nueva.getAltura())
-                && Objects.equals(original.getCiudad(), nueva.getCiudad())
-                && Objects.equals(original.getRegion(), nueva.getRegion())
-                && Objects.equals(original.getPais(), nueva.getPais());
+                && Objects.equals(original.getAltura(), nueva.getAltura());
     }
 
 }
