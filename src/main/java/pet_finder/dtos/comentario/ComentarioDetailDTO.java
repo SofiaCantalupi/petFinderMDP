75a9ce573351bd59e8af0f7cfa953d1e05f1235d
leoColmeck <leonel_colmeck2210@hotmail.com>
2026-06-25 21:37:46 -0300

    Agregar apellidoUsuario al detalle de comentarios

--- src/main/java/pet_finder/dtos/comentario/ComentarioDetailDTO.java
+++ src/main/java/pet_finder/dtos/comentario/ComentarioDetailDTO.java
@@ -6,7 +6,7 @@ import pet_finder.models.Comentario;
 import java.time.LocalDate;
 
 
-public record ComentarioDetailDTO (Long id, String texto, LocalDate fechaPublicacion, Boolean activo, Long idPublicacion, Long idMiembro, String nombreUsuario){
+public record ComentarioDetailDTO (Long id, String texto, LocalDate fechaPublicacion, Boolean activo, Long idPublicacion, Long idMiembro, String nombreUsuario, String apellidoUsuario){
 
 
     public ComentarioDetailDTO(Comentario comentario){
@@ -17,7 +17,8 @@ public record ComentarioDetailDTO (Long id, String texto, LocalDate fechaPublica
           comentario.getActivo(),
           comentario.getPublicacion().getId(),
           comentario.getMiembro().getId(),
-          comentario.getMiembro().getNombre()
+          comentario.getMiembro().getNombre(),
+          comentario.getMiembro().getApellido()
         );
     }
 }
