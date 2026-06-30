f6b0b0e015dcc5771a020f74bd3e8e650f897f48
SofiaCantalupi <sofiacantalupi@gmail.com>
2026-06-29 23:36:31 -0300

    Asociar valores a enums para la correcta normalizacion que requiere el frontend

--- src/main/java/pet_finder/enums/EstadoMascota.java
+++ src/main/java/pet_finder/enums/EstadoMascota.java
@@ -1,7 +1,18 @@
 package pet_finder.enums;
 
 public enum EstadoMascota {
-    PERDIDA,
-    ENCONTRADA,
-    REENCONTRADA
+    // Se asocia un valor string necesario para el frontend: en masculino y minuscula
+    PERDIDA("perdido"),
+    ENCONTRADA("encontrado"),
+    REENCONTRADA("reencontrado");
+
+    private final String valorFront;
+
+    EstadoMascota(String valorFront) {
+        this.valorFront = valorFront;
+    }
+
+    public String getValorFront() {
+        return valorFront;
+    }
 }
--- src/main/java/pet_finder/enums/TipoMascota.java
+++ src/main/java/pet_finder/enums/TipoMascota.java
@@ -1,6 +1,17 @@
 package pet_finder.enums;
 
 public enum TipoMascota {
-    PERRO,
-    GATO
+    // Se asocia un valor string necesario para el frontend: en miniscula
+    PERRO("perro"),
+    GATO("gato");
+
+    private final String valorFront;
+
+    TipoMascota(String valorFront){
+        this.valorFront = valorFront;
+    }
+
+    public String getValorFront(){
+        return valorFront;
+    }
 }
