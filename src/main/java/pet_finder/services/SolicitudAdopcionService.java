fd21b2997b6d0a057bb0dc3143ec4662fc607022
SofiaCantalupi <sofiacantalupi@gmail.com>
2026-07-14 16:54:24 -0300

    Crear metodo resolverSolicitudAdopcion() en SolicitudAdopcionService

--- src/main/java/pet_finder/services/SolicitudAdopcionService.java
+++ src/main/java/pet_finder/services/SolicitudAdopcionService.java
@@ -2,8 +2,12 @@ package pet_finder.services;
 
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
+import pet_finder.dtos.solicitud.ResolucionSolicitudRequestDTO;
 import pet_finder.dtos.solicitud.SolicitudAdopcionDetailDTO;
 import pet_finder.dtos.solicitud.SolicitudAdopcionRequestDTO;
+import pet_finder.enums.EstadoSolicitud;
+import pet_finder.enums.MotivoRechazo;
+import pet_finder.exceptions.OperacionNoPermitidaException;
 import pet_finder.mappers.SolicitudAdopcionMapper;
 import pet_finder.models.Publicacion;
 import pet_finder.models.SolicitudAdopcion;
@@ -11,6 +15,7 @@ import pet_finder.repositories.SolicitudAdopcionRepository;
 import pet_finder.validations.SolicitudAdopcionValidation;
 import pet_finder.models.Miembro;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -30,7 +35,7 @@ public class SolicitudAdopcionService {
     }
 
     @Transactional
-    public SolicitudAdopcionDetailDTO guardar(SolicitudAdopcionRequestDTO request, Long idMiembroSolicitante){
+    public SolicitudAdopcionDetailDTO guardar(SolicitudAdopcionRequestDTO request, Long idMiembroSolicitante) {
         // Se obtiene la publicacion completa
         Publicacion publicacion = publicacionService.obtenerPorId(request.getIdPublicacion());
         // Se obtiene el ID del miembro que hizo la publicacion
@@ -52,16 +57,48 @@ public class SolicitudAdopcionService {
     }
 
     @Transactional(readOnly = true)
-    public List<SolicitudAdopcionDetailDTO> listarRecibidas(Long idMiembroDuenio){
+    public List<SolicitudAdopcionDetailDTO> listarRecibidas(Long idMiembroDuenio) {
         List<SolicitudAdopcion> solicitudes = solicitudRepository.findByPublicacion_Miembro_IdAndPublicacion_ActivoTrue(idMiembroDuenio);
 
         return solicitudMapper.deEntidadesAdetails(solicitudes);
     }
 
     @Transactional(readOnly = true)
-    public List<SolicitudAdopcionDetailDTO> listarEnviadas(Long idMiembroSolicitante){
+    public List<SolicitudAdopcionDetailDTO> listarEnviadas(Long idMiembroSolicitante) {
         List<SolicitudAdopcion> solicitudes = solicitudRepository.findByMiembroSolicitante_IdAndPublicacion_ActivoTrue(idMiembroSolicitante);
 
         return solicitudMapper.deEntidadesAdetails(solicitudes);
+    }
+
+    @Transactional
+    public SolicitudAdopcionDetailDTO resolverSolicitud(Long idSolicitud, Long idMiembroLoggeado, ResolucionSolicitudRequestDTO resolucionRequest) {
+        SolicitudAdopcion solicitud = solicitudValidation.existePorId(idSolicitud);
+
+        if (!solicitud.getPublicacion().getMiembro().getId().equals(idMiembroLoggeado)) {
+            throw new OperacionNoPermitidaException("Solo el dueño de la publicación puede resolver la solicitud de adopción.");
+        }
+
+        if (!solicitud.getEstado().equals(EstadoSolicitud.PENDIENTE)){
+            throw new IllegalArgumentException("Esta solicitud ya fue resuelta.");
+        }
+
+        EstadoSolicitud nuevoEstado = solicitudValidation.validarYConvertirResolucion(resolucionRequest.getEstado());
+
+        solicitud.setEstado(nuevoEstado);
+        solicitud.setFechaResolucion(LocalDateTime.now());
+
+        if (nuevoEstado == EstadoSolicitud.RECHAZADA) {
+            solicitud.setMotivoRechazo(MotivoRechazo.MANUAL);
+
+            if (resolucionRequest.getComentarioResolucion() != null && !resolucionRequest.getComentarioResolucion().isBlank()) {
+                solicitud.setComentarioResolucion(resolucionRequest.getComentarioResolucion());
+            }
+        }
+
+        // AGREGAR METODO QUE CAMBIA EL ESTADO DE LA MASCOTA A ADOPTADO Y QUE CAMBIA EL ESTADO DE LAS OTRAS SOLICITUDES PENDIENTES A RECHAZADAS AUTO
+
+        SolicitudAdopcion guardada = solicitudRepository.save(solicitud);
+
+        return solicitudMapper.aDetail(guardada);
     }
 }
