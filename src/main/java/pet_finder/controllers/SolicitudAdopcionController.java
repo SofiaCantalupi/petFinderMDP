6eb7e0e0f19461f6d34d3dcd714931fdd32ac3c5
SofiaCantalupi <sofiacantalupi@gmail.com>
2026-07-20 11:46:38 -0300

    Crear resolverSolicitud() en SolicitudAdopcionController

--- src/main/java/pet_finder/controllers/SolicitudAdopcionController.java
+++ src/main/java/pet_finder/controllers/SolicitudAdopcionController.java
@@ -7,6 +7,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import pet_finder.config.MiembroUserDetails;
+import pet_finder.dtos.solicitud.ResolucionSolicitudRequestDTO;
 import pet_finder.dtos.solicitud.SolicitudAdopcionDetailDTO;
 import pet_finder.dtos.solicitud.SolicitudAdopcionRequestDTO;
 import pet_finder.services.SolicitudAdopcionService;
@@ -43,5 +44,14 @@ public class SolicitudAdopcionController {
     public ResponseEntity<List<SolicitudAdopcionDetailDTO>> listarEnviadas(@AuthenticationPrincipal MiembroUserDetails userDetails){
         List<SolicitudAdopcionDetailDTO> dtos =  solicitudService.listarEnviadas(userDetails.getId());
         return  ResponseEntity.ok(dtos);
+    }
+
+    @PreAuthorize("hasRole('MIEMBRO')")
+    @PutMapping("/estado/{id}")
+    public ResponseEntity<SolicitudAdopcionDetailDTO> resolver(@PathVariable Long id,
+                                                                                @AuthenticationPrincipal MiembroUserDetails userDetails,
+                                                                                @Valid @RequestBody ResolucionSolicitudRequestDTO requestDTO){
+        SolicitudAdopcionDetailDTO resuelta = solicitudService.resolverSolicitudAdopcion(id,userDetails.getId(),requestDTO);
+        return ResponseEntity.ok(resuelta);
     }
 }
